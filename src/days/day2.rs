d8752b5d542ade502d9c03f4ce0930cde0a5face
Alberto <albgp22@gmail.com>
2023-03-22 20:05:56 +0100

    Day 2

---
+++ src/days/day2.rs
@@ -0,0 +1,86 @@
+use std::str::FromStr;
+
+use crate::problem::problemdef::Problem;
+
+pub struct DayTwo {}
+
+enum Instruction{
+    Forward(i64),
+    Down(i64),
+    Up(i64),
+}
+
+#[derive(Debug)]
+struct ParseInstructionError;
+
+impl FromStr for Instruction {
+    type Err = ParseInstructionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.clone().split(" ");
+        let ins_base = parts.next().ok_or(ParseInstructionError)?;
+        let qua_st = parts.next().ok_or(ParseInstructionError)?;
+        let qua: i64 = qua_st.parse().unwrap();
+        let ins = match ins_base{
+            "forward" => Instruction::Forward(qua),
+            "down" => Instruction::Down(qua),
+            "up" => Instruction::Up(qua),
+            _ => return Err(ParseInstructionError),
+        };
+
+        Ok(ins)
+    }
+}
+
+impl Problem for DayTwo {
+    fn part_one(&self, input: &str) -> String {
+        let mut x = 0;
+        let mut y = 0;
+
+        input
+            .split("\n")
+            .into_iter()
+            .filter(|s| !s.is_empty())
+            .map(|line| line.parse::<Instruction>().unwrap())
+            .for_each(
+                |i|
+                match i {
+                    Instruction::Forward(q) => x+=q,
+                    Instruction::Down(q) => y+=q,
+                    Instruction::Up(q) => y-=q,
+                }
+            );
+
+        format!("{}", x*y)
+    }
+
+    fn part_two(&self, input: &str) -> String {
+        let mut x = 0;
+        let mut y = 0;
+        let mut aim = 0;
+
+        input
+            .split("\n")
+            .into_iter()
+            .filter(|s| !s.is_empty())
+            .map(|line| line.parse::<Instruction>().unwrap())
+            .for_each(
+                |i|
+                match i {
+                    Instruction::Forward(q) => {
+                        x+=q;
+                        y+=aim*q;
+                    }
+                    Instruction::Down(q) => aim+=q,
+                    Instruction::Up(q) => aim-=q,
+                }
+            );
+
+        format!("{}", x*y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod problem;
 mod days;
 
 use problem::problemdef::Problem;
-use days::day1::DayOne;
+use days::*;
 use clap::Parser;
 use std::fs;
 use std::error::Error;
@@ -18,7 +18,8 @@ struct Args {
 
 fn day_to_problem(day: usize) -> Option<Box<dyn Problem>> {
     match day {
-        1 => Some(Box::new(DayOne{})),
+        1 => Some(Box::new(day1::DayOne{})),
+        2 => Some(Box::new(day2::DayTwo{})),
         _ => None
     }
 }
@@ -28,7 +29,7 @@ fn main() -> Result<(), Box<dyn Error>>{
     let args = Args::parse();
 
     let problem = day_to_problem(args.day).ok_or("Day is not implemented")?;
-    let file_content = fs::read_to_string(format!("inputs/{}.txt",args.day))?;
+    let file_content = fs::read_to_string(format!("src/inputs/{}.txt",args.day))?;
 
     problem.run(&file_content);
 
