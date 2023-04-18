ab083a3182849984e79e34f4943c7fee25aef60a
albgp22 <albgp22@gmail.com>
2023-04-18 07:39:39 +0200

    Add json output for benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-mod problem;
 mod days;
+mod problem;
 
-use problem::problemdef::Problem;
-use days::*;
 use clap::Parser;
-use std::fs;
+use days::*;
+use problem::problemdef::{BenchmarkResult, Problem};
 use std::error::Error;
+use std::fs;
+
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
 
 const NOPROBLEM: usize = 1234;
 
-#[derive(Parser,Debug)]
+#[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long, default_value_t = NOPROBLEM)]
     day: usize,
@@ -21,61 +27,80 @@ struct Args {
     run_all: bool,
 }
 
-
 fn day_to_problem(day: usize) -> Option<Box<dyn Problem>> {
     match day {
-        1 => Some(Box::new(day1::DayOne{})),
-        2 => Some(Box::new(day2::DayTwo{})),
-        3 => Some(Box::new(day3::DayThree{})),
-        4 => Some(Box::new(day4::DayFour{})),
-        5 => Some(Box::new(day5::DayFive{})),
-        6 => Some(Box::new(day6::DaySix{})),
-        7 => Some(Box::new(day7::DaySeven{})),
-        8 => Some(Box::new(day8::DayEight{})),
-        9 => Some(Box::new(day9::DayNine{})),
-        10 => Some(Box::new(day10::DayTen{})),
-        11 => Some(Box::new(day11::DayEleven{})),
-        12 => Some(Box::new(day12::DayTwelve{})),
-        13 => Some(Box::new(day13::DayThirteen{})),
-        _ => None
+        1 => Some(Box::new(day1::DayOne {})),
+        2 => Some(Box::new(day2::DayTwo {})),
+        3 => Some(Box::new(day3::DayThree {})),
+        4 => Some(Box::new(day4::DayFour {})),
+        5 => Some(Box::new(day5::DayFive {})),
+        6 => Some(Box::new(day6::DaySix {})),
+        7 => Some(Box::new(day7::DaySeven {})),
+        8 => Some(Box::new(day8::DayEight {})),
+        9 => Some(Box::new(day9::DayNine {})),
+        10 => Some(Box::new(day10::DayTen {})),
+        11 => Some(Box::new(day11::DayEleven {})),
+        12 => Some(Box::new(day12::DayTwelve {})),
+        13 => Some(Box::new(day13::DayThirteen {})),
+        _ => None,
     }
 }
 
-fn run_problem(p: Box<dyn Problem>, benchmark: bool, day: usize) -> Result<(), Box<dyn Error>>{
-
-    let file_content = fs::read_to_string(format!("src/inputs/{}.txt",day))?;
+fn run_problem(
+    p: Box<dyn Problem>,
+    benchmark: bool,
+    day: usize,
+) -> Result<Option<BenchmarkResult>, Box<dyn Error>> {
+    let file_content = fs::read_to_string(format!("src/inputs/{}.txt", day))?;
     p.run(&file_content);
 
-    if benchmark{
-        p.benchmark(&file_content);
+    if benchmark {
+        return Ok(Some(p.benchmark(&file_content)));
     }
 
-    Ok(())
+    Ok(None)
 }
 
-fn main() -> Result<(), Box<dyn Error>>{
-
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-
-    if args.day==NOPROBLEM && !args.run_all{
+    if args.day == NOPROBLEM && !args.run_all {
         return Err("Either --day <day> or --run-all must be provided.".into());
     }
 
-    if args.run_all{
-        (0..=25).for_each(|i|{
+    if args.run_all {
+        let mut bench_results = vec![];
+
+        for i in 0..=25 {
             let p = day_to_problem(i);
-            if p.is_some(){
+            if p.is_some() {
                 println!("Day {}:", i);
-                run_problem(p.unwrap(), args.benchmark, i).unwrap();
+                match run_problem(p.unwrap(), args.benchmark, i) {
+                    Ok(Some(br)) => {
+                        bench_results.push(br);
+                        bench_results.last_mut().unwrap().set_problem_num(i)
+                    }
+                    Ok(None) => {}
+                    Err(e) => return Err(e),
+                }
                 println!("");
             }
-        })
-    }else{
+        }
+        if args.benchmark {
+            let serialized = serde_json::to_string(&bench_results).unwrap();
+            println!("{}", serialized);
+        }
+    } else {
         let p = day_to_problem(args.day).ok_or("Day is not implemented")?;
-        return run_problem(p, args.benchmark, args.day)
+        match run_problem(p, args.benchmark, args.day) {
+            Ok(Some(br)) => {
+                let serialized = serde_json::to_string_pretty(&br).unwrap();
+                println!("{}", serialized);
+            }
+            Ok(None) => {}
+            Err(e) => return Err(e),
+        }
     }
-
 
     Ok(())
 }
--- src/problem/problemdef.rs
+++ src/problem/problemdef.rs
@@ -1,37 +1,73 @@
-
 use std::time::Instant;
 
-pub trait Problem{
+pub trait Problem {
     fn part_one(&self, input: &str) -> String;
     fn part_two(&self, input: &str) -> String;
 }
 
-impl dyn Problem{
-    pub fn run(&self, input: &str){
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BenchmarkResult {
+    problem: usize,
+    //durations: Vec<Duration>,
+    mean_millis: f64,
+    var_millis_squared: f64,
+}
+
+impl BenchmarkResult {
+    pub fn set_problem_num(&mut self, i: usize) {
+        self.problem = i;
+    }
+}
+
+impl dyn Problem {
+    pub fn run(&self, input: &str) {
         println!["Part 1 solution:"];
-        println!["{}",self.part_one(input)];
+        println!["{}", self.part_one(input)];
         println!["Part 2 solution:"];
-        println!["{}",self.part_two(input)];
+        println!["{}", self.part_two(input)];
     }
 
-    pub fn benchmark(&self, input: &str){
+    pub fn benchmark(&self, input: &str) -> BenchmarkResult {
         let now = Instant::now();
-        const N: u32 = 100;
+        const N: u32 = 10;
 
-        for _ in 0..N{
+        let mut durations = vec![];
+
+        for _ in 0..N {
+            let now = Instant::now();
             self.part_one(input);
+            let elapsed = now.elapsed();
+            durations.push(elapsed);
         }
 
         let elapsed = now.elapsed();
-        println!("Part 1 took {:.5?} on average", elapsed/N);
+        println!("Part 1 took {:.5?} on average", elapsed / N);
 
         let now = Instant::now();
 
-        for _ in 0..N{
+        for _ in 0..N {
             self.part_two(input);
         }
 
         let elapsed = now.elapsed();
-        println!("Part 2 took {:.5?} on average", elapsed/N);
+        println!("Part 2 took {:.5?} on average", elapsed / N);
+
+        let mean = durations
+            .iter()
+            .map(|f| f.as_secs_f64() * (1000 as f64))
+            .fold(0f64, |x, y| x + y)
+            / (durations.len() as f64);
+        let var = durations
+            .iter()
+            .map(|f| f.as_secs_f64() * (1000 as f64))
+            .fold(0f64, |x, y| x + (mean - y).powi(2))
+            / (durations.len() as f64);
+
+        BenchmarkResult {
+            problem: 0,
+            //durations: durations,
+            mean_millis: mean,
+            var_millis_squared: var,
+        }
     }
 }
