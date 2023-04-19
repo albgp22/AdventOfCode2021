2bf238e5de8a19d8e773c59b4de957691b4233f2
albgp22 <albgp22@gmail.com>
2023-04-19 07:50:38 +0200

    Print stats also for part 2

--- src/problem/problemdef.rs
+++ src/problem/problemdef.rs
@@ -9,8 +9,10 @@ pub trait Problem {
 pub struct BenchmarkResult {
     problem: usize,
     //durations: Vec<Duration>,
-    mean_millis: f64,
-    var_millis_squared: f64,
+    mean_millis_pt1: f64,
+    var_millis_squared_pt1: f64,
+    mean_millis_pt2: f64,
+    var_millis_squared_pt2: f64,
 }
 
 impl BenchmarkResult {
@@ -31,13 +33,14 @@ impl dyn Problem {
         let now = Instant::now();
         const N: u32 = 10;
 
-        let mut durations = vec![];
+        let mut durations1 = vec![];
+        let mut durations2 = vec![];
 
         for _ in 0..N {
             let now = Instant::now();
             self.part_one(input);
             let elapsed = now.elapsed();
-            durations.push(elapsed);
+            durations1.push(elapsed);
         }
 
         let elapsed = now.elapsed();
@@ -46,28 +49,43 @@ impl dyn Problem {
         let now = Instant::now();
 
         for _ in 0..N {
+            let now = Instant::now();
             self.part_two(input);
+            let elapsed = now.elapsed();
+            durations2.push(elapsed);
         }
 
         let elapsed = now.elapsed();
         println!("Part 2 took {:.5?} on average", elapsed / N);
 
-        let mean = durations
+        let mean1 = durations1
             .iter()
             .map(|f| f.as_secs_f64() * (1000 as f64))
             .fold(0f64, |x, y| x + y)
-            / (durations.len() as f64);
-        let var = durations
+            / (durations1.len() as f64);
+        let var1 = durations1
             .iter()
             .map(|f| f.as_secs_f64() * (1000 as f64))
-            .fold(0f64, |x, y| x + (mean - y).powi(2))
-            / (durations.len() as f64);
+            .fold(0f64, |x, y| x + (mean1 - y).powi(2))
+            / (durations1.len() as f64);
+        let mean2 = durations2
+            .iter()
+            .map(|f| f.as_secs_f64() * (1000 as f64))
+            .fold(0f64, |x, y| x + y)
+            / (durations2.len() as f64);
+        let var2 = durations2
+            .iter()
+            .map(|f| f.as_secs_f64() * (1000 as f64))
+            .fold(0f64, |x, y| x + (mean2 - y).powi(2))
+            / (durations2.len() as f64);
 
         BenchmarkResult {
             problem: 0,
             //durations: durations,
-            mean_millis: mean,
-            var_millis_squared: var,
+            mean_millis_pt1: mean1,
+            var_millis_squared_pt1: var1,
+            mean_millis_pt2: mean2,
+            var_millis_squared_pt2: var2,
         }
     }
 }
